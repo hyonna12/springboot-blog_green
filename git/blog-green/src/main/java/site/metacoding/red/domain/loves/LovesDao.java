d59fbe157649ed65d7d84f39efd524fecb72b58a
hyonna12 <hyonna01@gmail.com>
2022-09-19 18:14:38 +0900

    좋아요 쿼리 수정 - 한방 쿼리로 변경

--- git/blog-green/src/main/java/site/metacoding/red/domain/loves/LovesDao.java
+++ git/blog-green/src/main/java/site/metacoding/red/domain/loves/LovesDao.java
@@ -2,9 +2,6 @@ package site.metacoding.red.domain.loves;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
-
-import site.metacoding.red.web.dto.response.loves.LovesDto;
 
 public interface LovesDao {
 	public void insert(Loves loves);
@@ -12,5 +9,4 @@ public interface LovesDao {
 	public Loves findById(Integer id);
 	public void update(Loves loves);
 	public void deleteById(Integer id);
-	public LovesDto findByBoardsId(@Param("boardsId") Integer boardsId, @Param("principalId") Integer principalId);
 }
